53f612389d70db949f3b1b543947b1530a32d346
Shingo Fukuyama <fukuyama012@gmail.com>
2019-01-14 02:24:56 +0900

    Userモデル修正

--- service/web/app/models/user_test.go
+++ service/web/app/models/user_test.go
@@ -148,7 +148,7 @@ func TestUser_DeleteUserById(t *testing.T) {
 		}
 		
 		user := models.User{}
-		err := user.DeleteUserById(tt.in);
+		err := user.DeleteById(tt.in);
 		if err != nil {
 			t.Errorf("User Delete err %#v", err)
 		}
@@ -183,10 +183,8 @@ func TestUser_DeleteUserByIdZeroValueError(t *testing.T) {
 		}
 
 		user := models.User{}
-		err := user.DeleteUserById(tt.in);
-		if err == nil {
-			t.Errorf("cant detect Delete err %#v", err)
-		}
+		err := user.DeleteById(tt.in);
+		assert.Error(t, err)
 		recordCountAfter, errCount := models.CountUser()
 		if errCount != nil {
 			t.Errorf("user count err %#v", errCount)
