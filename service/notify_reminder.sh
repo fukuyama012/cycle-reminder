d0c79140f3a68a0706b143ae927fed30b9675590
Shingo Fukuyama <fukuyama012@gmail.com>
2019-01-31 00:01:16 +0900

    cronシェルスクリプト内のコマンドはパスが通っていないので絶対パス記述

--- service/notify_reminder.sh
+++ service/notify_reminder.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # cron exec
-# example) 0 12-18/2 * * * bash ~/cycle-reminder/service/notify_reminder.sh
-docker-compose -f ~/cycle-reminder/service/docker-compose.yml exec web go run app/cron/notifyreminder.go
+# example) 0 12-18/2 * * * bash ~/cycle-reminder/service/notify_reminder.sh 2>> ~/cron_err.log
+/usr/local/bin/docker-compose -f ~/cycle-reminder/service/docker-compose.yml exec web go run app/cron/notifyreminder.go
